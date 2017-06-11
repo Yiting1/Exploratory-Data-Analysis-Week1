1c6951c50830a3682f11722ba367c1bbae389bd9
Yiting1 <ytwangzd@tsmc.com>
2017-06-11 20:43:30 +0800

    Update

--- plot4.R
+++ plot4.R
@@ -4,7 +4,7 @@ data <- fread("household_power_consumption.txt")
 data <- as.data.frame(data)
 data$Date <- as.Date(data$Date,"%d/%m/%Y")
 data <- data %>% filter(Date=="2007-02-01" | Date=="2007-02-02") %>% 
-  mutate(datetime=paste(Date,Time,sep=" "))
+  mutate(datetime=as.POSIXct(paste(Date,Time,sep=" ")))
 
 
 par(mfrow = c(2,2))
